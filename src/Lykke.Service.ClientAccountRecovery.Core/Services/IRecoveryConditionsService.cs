cc9fb8c592e58baeaa9c68781a9f5e3313528c7f
Georgii Afanasev <gmafsgu@gmail.com>
2018-09-03 16:11:05 +0400

    Fix RecoveryFlowServiceFactory.

---
+++ src/Lykke.Service.ClientAccountRecovery.Core/Services/IRecoveryConditionsService.cs
@@ -0,0 +1,15 @@
+﻿using Lykke.Service.ClientAccountRecovery.Core.Domain;
+
+namespace Lykke.Service.ClientAccountRecovery.Core.Services
+{
+    /// <summary>
+    ///     Provide recovery conditions for other services.
+    /// </summary>
+    public interface IRecoveryConditionsService
+    {
+        /// <summary>
+        ///     Recovery conditions.
+        /// </summary>
+        RecoveryConditions RecoveryConditions { get; }
+    }
+}
---
+++ src/Lykke.Service.ClientAccountRecovery.Services/RecoveryConditionsService.cs
@@ -0,0 +1,17 @@
+﻿using Lykke.Service.ClientAccountRecovery.Core.Domain;
+using Lykke.Service.ClientAccountRecovery.Core.Services;
+
+namespace Lykke.Service.ClientAccountRecovery.Services
+{
+    /// <inheritdoc />
+    public class RecoveryConditionsService : IRecoveryConditionsService
+    {
+        public RecoveryConditionsService(RecoveryConditions recoveryConditions)
+        {
+            RecoveryConditions = recoveryConditions;
+        }
+
+        /// <inheritdoc />
+        public RecoveryConditions RecoveryConditions { get; }
+    }
+}
